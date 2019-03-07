89395cf7ce17b9232e5d2219e6d335a01f1836ec
Philipp Schlegel <observing@web.de>
2019-03-06 22:28:10 -0500

    new function: utils.gen_assigments

--- dvidtools/utils.py
+++ dvidtools/utils.py
@@ -1,6 +1,12 @@
 # This code is part of dvid-tools (https://github.com/flyconnectome/dvid_tools)
 # and is released under GNU GPL3
 
+import json
+import pandas as pd
+
+from io import StringIO
+
+
 def verify_payload(data, required, required_only=True):
     """ Verifies payload.
 
@@ -12,7 +18,7 @@ def verify_payload(data, required, required_only=True):
                 Required entries. Can be nested. For example::
                     {'Note': str, 'User': str, 'Props': {'name': str}}
 
-    Returns 
+    Returns
     -------
     None
     """
@@ -27,8 +33,8 @@ def verify_payload(data, required, required_only=True):
 
         for e, t in required.items():
             if isinstance(t, dict):
-                verify_payload([d[e]], t)            
-            else:                
+                verify_payload([d[e]], t)
+            else:
                 if e not in d:
                     raise ValueError('Data must contain entry "{}"'.format(e))
                 if isinstance(t, type) and not isinstance(d[e], t):
@@ -71,6 +77,52 @@ def parse_swc_str(x):
 
     return df, header
 
+
+def gen_assignments(x, save_to=None, meta={}):
+    """ Generates JSON file that can be imported into neutu as assignments.
+
+    Parameters
+    ----------
+    x :         pandas.DataFrame
+                Must contain columns ``x``, ``y``, ``z`` or ``location``.
+                Optional columns: ``body ID`` and ``text``.
+    save_to :   None | filepath | filebuffer, optional
+                If not None, will save json to file.
+    meta :      dict, optional
+                Metadata will be stored in json string as ``"metadata"``.
+
+    Returns
+    -------
+    JSON-formated string
+                Only if ``save_to=None``.
+    """
+
+    if not isinstance(x, pd.DataFrame):
+        raise TypeError('x must be pandas DataFrame, got "{}"'.format(type(x)))
+
+    if 'location' not in x.columns:
+        if any([c not in  x.columns for c in ['x', 'y', 'z']]):
+            raise ValueError('x must have "location" column or "x", "y" '
+                             'and "z" columns')
+        x['location'] = x[['x','y','z']].astype(int).apply(list, axis=1)
+
+    for c in ['body ID', 'text']:
+        if c not in x.columns:
+            x[c] = ''
+
+    x = x[['location', 'text', 'body ID']]
+
+    j = {'metadata': meta,
+         'data': x.to_dict(orient='records')}
+
+    if save_to:
+        with open(save_to, 'w') as f:
+            json.dump(j, f, indent=2)
+    else:
+        return j
+
+
+def parse_bid(x):
     try:
         return int(x)
     except:
