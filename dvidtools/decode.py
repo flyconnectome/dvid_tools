71c13fe7486b82fc8b62dd64b255e14430df9a70
Philipp Schlegel <observing@web.de>
2019-03-06 19:01:26 -0500

    fix decode binary mesh format Didn't take run length into account

--- dvidtools/decode.py
+++ dvidtools/decode.py
@@ -12,22 +12,20 @@ def decode_sparsevol(b, format='rles'):
     Parameters
     ----------
     b :         bytes
-                Data to decode.      
+                Data to decode.
     format :    "blocks" | "rles"
-                Binary format in which the file is encoded. Only "rles" is 
+                Binary format in which the file is encoded. Only "rles" is
                 supported at the moment.
 
     Returns
     -------
-    voxel coordinates
+    voxel indices
                 Please note that this function is agnostic to voxel size, etc.
-    
+
     """
     if not isinstance(b, bytes):
         raise TypeError('Need bytes, got "{}"'.format(type(b)))
-    
-    
-          
+
     if format == 'rles':
         # First get the header
         header = {k: v for k, v in zip(['start_byte', 'n_dims', 'run_dims',
@@ -37,8 +35,12 @@ def decode_sparsevol(b, format='rles'):
         for i in range(header['n_spans']):
             offset = 12 + (i * 16)
             x, y, z, run_len = struct.unpack('iiii', b[offset: offset + 16])
-            coords.append([x,y,z])
-        coords = np.array(coords)
+
+            this_run = np.repeat([[x, y, z]], run_len, axis=0)
+            this_run[:, 0] = np.arange(x, x + run_len)
+
+            coords.append(this_run)
+        coords = np.concatenate(coords)
         return header, coords
     elif format == 'blocks':
         raise ValueError('Format "blocks" not yet implemented.')
