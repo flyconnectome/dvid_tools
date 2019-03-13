75e294fe51bb92752b9f3990f6263fe341bc5f63
Philipp Schlegel <observing@web.de>
2019-03-13 10:04:57 -0400

    added mesh utility functions also fix mistake in parsing rles blocks (had x,y,z1,z2 instead of z,y,x1,x2)

--- dvidtools/mesh.py
+++ dvidtools/mesh.py
@@ -4,7 +4,8 @@
 import numpy as np
 
 from skimage.measure import marching_cubes_lewiner
-from scipy.ndimage.morphology import binary_erosion
+from scipy.ndimage.morphology import binary_erosion, binary_fill_holes
+
 
 def mesh_from_voxels(voxels, v_size, step_size=1):
     """ Turns voxels into vertices and faces using marching cubes.
@@ -14,7 +15,7 @@ def mesh_from_voxels(voxels, v_size, step_size=1):
     voxels :    np.array
                 Voxel coordindates. Array of either (N, 3) or (N, 4).
                 (N, 3) will be interpreted as single x, y, z voxel coordinates.
-                (N, 4) will be interpreted as column of x, y, z_start, z_end
+                (N, 4) will be interpreted as blocks of z, y, x_start, x_end
                 coordindates.
     v_size :    np.array
                 (3, ) array with x, y, z voxel size.
@@ -25,20 +26,11 @@ def mesh_from_voxels(voxels, v_size, step_size=1):
     """
     # Offset block coordinates by +1.
     # Necessary for marching cube producing watertight meshes.
-    # Will compensate for this further down
+    # Will compensate for this later
     voxels += 1
 
-    # Generate empty array
-    mat = np.zeros((voxels.max(axis=0) + 2)[[0, 1, -1]], dtype=np.float32)
-
-    #Populate matrix
-    if voxels.shape[1] == 4:
-        for col in voxels:
-            mat[col[0], col[1], col[2]:col[3]] = 1
-    elif voxels.shape[1] == 3:
-        mat[voxels[:, 0], voxels[:, 1], voxels[:, 2]] = 1
-    else:
-        raise ValueError('Unexpected shape')
+    # Turn voxels into matrix
+    mat = _voxels_to_matrix(voxels)
 
     # Use marching cubes to create surface model
     verts, faces, normals, values = marching_cubes_lewiner(mat,
@@ -48,11 +40,8 @@ def mesh_from_voxels(voxels, v_size, step_size=1):
                                                            gradient_direction='ascent',
                                                            spacing=v_size)
 
-    # Compensate for earlier block offset
+    # Compensate for earlier voxel offset
     verts -= np.array(v_size)
-
-    # For unknown reasons, the x/y/z order is inverted during marching cubes
-    verts = verts[:, [2, 1, 0]]
 
     return verts, faces
 
@@ -63,27 +52,145 @@ def remove_surface_voxels(voxels, **kwargs):
     # Use bounding boxes to keep matrix small
     bb_min = voxels.min(axis=0)
     bb_max = voxels.max(axis=0)
-    dim = (bb_max - bb_min) + 1
+    dim = bb_max - bb_min
 
     # Voxel offset
     voxel_off = voxels - bb_min
 
     # Generate empty array
-    mat = np.zeros(dim + 1, dtype=np.float32)
-
-    #Populate matrix
-    if voxels.shape[1] == 3:
-        mat[voxel_off[:, 0], voxel_off[:, 1], voxel_off[:, 2]] = 1
-    else:
-        raise ValueError('Unexpected shape')
+    mat = _voxels_to_matrix(voxel_off)
 
     # Erode
-    mat_erode = binary_erosion(mat, **kwargs).astype(mat.dtype)
+    mat_erode = binary_erosion(mat, **kwargs)
 
     # Turn back into voxels
-    voxels_erode = np.vstack(np.where(mat_erode == 1)).T + bb_min
+    voxels_erode = _matrix_to_voxels(mat_erode) + bb_min
 
     return voxels_erode
 
+
+def get_surface_voxels(voxels):
+    """ Returns surface voxels. """
+
+    # Use bounding boxes to keep matrix small
+    bb_min = voxels.min(axis=0)
+    bb_max = voxels.max(axis=0)
+    dim = bb_max - bb_min
+
+    # Voxel offset
+    voxel_off = voxels - bb_min
+
+    # Generate empty array
+    mat = _voxels_to_matrix(voxel_off)
+
+    # Erode
+    mat_erode = binary_erosion(mat)
+
+    # Substract
+    mat_surface = np.bitwise_and(mat, np.invert(mat_erode))
+
+    # Turn back into voxels
+    voxels_surface = _matrix_to_voxels(mat_surface) + bb_min
+
+    return voxels_surface
+
+
+def parse_obj(obj):
+    """ Parse .obj string and return vertices and faces.
+    """
+
+    lines = obj.split('\n')
+    verts = []
+    faces = []
+    for l in lines:
+        if l.startswith('v '):
+            verts.append([float(v) for v in  l[2:].split(' ')])
+        elif l.startswith('f '):
+            f = [v.split('//')[0] for v in l[2:].split(' ')]
+            faces.append([int(v) for v in f])
+
+    # `.obj` faces start with vertex indices of 1 -> set to 0
+    return np.array(verts), np.array(faces) - 1
+
+
+def _voxels_to_matrix(voxels, fill=False):
+    """ Generate matrix from voxels/blocks.
+
+    Parameters
+    ----------
+    voxels :    numpy array
+                Either voxels [[x, y, z], ..] or blocks [[z, y, x1, x2], ...]
+    fill :      bool, optional
+                If True, will use binary fill to fill holes in matrix.
+
+    Returns
+    -------
+    numpy array
+                3D matrix with x, y, z axis (blocks will be converted)
+    """
+
+    if not isinstance(voxels, np.ndarray):
+        voxels = np.array(voxels)
+
+    #Populate matrix
+    if voxels.shape[1] == 4:
+        mat = np.zeros((voxels.max(axis=0) + 2)[[-1, 1, 0]], dtype=np.bool)
+        for col in voxels:
+            mat[col[2]:col[3]+1, col[1], col[0]] = 1
+    elif voxels.shape[1] == 3:
+        mat = np.zeros((voxels.max(axis=0) + 2), dtype=np.bool)
+        mat[voxels[:, 0], voxels[:, 1], voxels[:, 2]] = 1
+    else:
+        raise ValueError('Unexpected voxel shape')
+
+    # Fill holes
+    if fill:
+        mat = binary_fill_holes(mat)
+
+    return mat
+
+
+def _matrix_to_voxels(matrix):
+    """ Turns matrix into voxels. Assumes that voxels have values True or 1.
+    """
+
+    # Turn matrix into voxels
+    return np.vstack(np.where(matrix)).T
+
+
+def _apply_mask(mat, mask):
+    """ Substracts (logical_xor) mask from mat. Assumes that both matrices
+    are (a) of the same voxel size and (b) have the same origin.
+    """
+
+    # Get maximum dimension between mat and mask
+    max_dim = np.max(np.array([mat.shape, mask.shape]), axis=0)
+
+    # Bring mask in shape of mat
+    mask_pad = np.vstack( [np.array([0, 0, 0]), max_dim - mask.shape]).T
+    mask = np.pad(mask, mask_pad, mode='constant', constant_values=0)
+    mask = mask[:mat.shape[0], :mat.shape[1], :mat.shape[2]]
+
+    # Substract mask
+    return np.bitwise_and(mat, np.invert(mask))
+
+
+def _mask_voxels(voxels, mask_voxels):
+    """ Mask voxels with other voxels. Assumes that both voxels have the
+    same size and origin."""
+
+    # Generate matrices of the voxels
+    mat = _voxels_to_matrix(voxels)
+    mask = _voxels_to_matrix(mask_voxels)
+
+    # Apply mask
+    masked = _apply_mask(mat, mask)
+
+    # Turn matrix back into voxels
+    return _matrix_to_voxels(masked)
+
+
+def _blocks_to_voxels(blocks):
+    return _matrix_to_voxels(_voxels_to_matrix(blocks))
 
 
