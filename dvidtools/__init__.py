4bddccc5ff6b3fe2c8c87afc674b35bb08e02a75
Philipp Schlegel <observing@web.de>
2019-05-17 09:32:57 +0100

    bump version 0.1.7->0.1.8

--- dvidtools/__init__.py
+++ dvidtools/__init__.py
@@ -1,8 +1,8 @@
 # This code is part of dvid-tools (https://github.com/flyconnectome/dvid_tools)
 # and is released under GNU GPL3
 
-__version__ = (0, 1, 7)
-__verstr__ = "0.1.7"
+__version__ = (0, 1, 8)
+__verstr__ = "0.1.8"
 
 from .fetch import *
 from .tip import *
