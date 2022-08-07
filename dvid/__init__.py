3461afcf90fac82be50c5b2dd22bb298ff6a0766
Philipp Schlegel <observing@web.de>
2022-08-07 10:05:03 +0100

    bump version 0.3.0 -> 0.4.0

--- dvid/__init__.py
+++ dvid/__init__.py
@@ -1,8 +1,8 @@
 # This code is part of dvid-tools (https://github.com/flyconnectome/dvid_tools)
 # and is released under GNU GPL3
 
-__version__ = (0, 3, 0)
-__verstr__ = "0.3.0"
+__version__ = (0, 4, 0)
+__verstr__ = "0.4.0"
 
 from .fetch import *
 from .tip import *
