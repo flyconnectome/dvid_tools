003286fc79756b3fb10516572a78f0980a60697b
Philipp Schlegel <observing@web.de>
2022-08-07 10:01:36 +0100

    decode_sparsevol: new parameter to allow returning RLES instead of voxels

--- dvid/decode.py
+++ dvid/decode.py
@@ -9,7 +9,7 @@ import trimesh as tm
 from io import BytesIO
 
 
-def decode_sparsevol(b, format='rles'):
+def decode_sparsevol(b, format='rles', voxels=True):
     """Decode sparsevol binary mesh format.
 
     Parameters
@@ -19,6 +19,8 @@ def decode_sparsevol(b, format='rles'):
     format :    "blocks" | "rles"
                 Binary format in which the file is encoded. Only "rles" is
                 supported at the moment.
+    voxels :    bool
+                Whether to return x/y/z voxels or original x/y/z/x_run_length rles.
 
     Returns
     -------
@@ -34,20 +36,27 @@ def decode_sparsevol(b, format='rles'):
         header = {k: v for k, v in zip(['start_byte', 'n_dims', 'run_dims',
                                         'reserved', 'n_blocks', 'n_spans'],
                                        struct.unpack('bbbbii', b[:12]))}
-        coords = []
-        for i in range(header['n_spans']):
-            offset = 12 + (i * 16)
-            x, y, z, run_len = struct.unpack('iiii', b[offset: offset + 16])
 
-            this_run = np.array([[x, y, z]] * run_len)
-            this_run[:, 0] = np.arange(x, x + run_len)
+        if voxels:
+            coords = []
+            for i in range(header['n_spans']):
+                offset = 12 + (i * 16)
+                x, y, z, run_len = struct.unpack('iiii', b[offset: offset + 16])
 
-            coords.append(this_run)
+                this_run = np.array([[x, y, z]] * run_len, dtype='uint32')
+                this_run[:, 0] = np.arange(x, x + run_len)
 
-        if len(coords):
-            coords = np.concatenate(coords)
+                coords.append(this_run)
+
+            if len(coords):
+                coords = np.concatenate(coords)
+            else:
+                coords = np.zeros((0, 3))
         else:
-            coords = np.zeros((0, 3))
+            coords = np.zeros((header['n_spans'], 4), dtype='uint32')
+            for i in range(header['n_spans']):
+                offset = 12 + (i * 16)
+                coords[i, :] = struct.unpack('iiii', b[offset: offset + 16])
         return header, coords
     elif format == 'blocks':
         raise ValueError('Format "blocks" not yet implemented.')
